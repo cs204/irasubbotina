15c5b63f6898ee21f42c9faa514d0d0ae21fd16d
irasubbotina <irulik.subbotina@mail.ru>
2021-05-12 08:03:12 +0000

    first commit

---
+++ agenda.js
@@ -0,0 +1,25 @@
+module.exports = class Agenda extends Array
+{
+    add(node)
+    {
+        this.push(node)
+    }
+
+    getNode()
+    {
+
+		return this.pop()
+    }
+
+    notEmpty()
+    {
+        return this.length !== 0
+    }
+
+
+}
+
+
+
+
+
